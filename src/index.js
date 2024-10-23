740fd8a807cb8855151083981512a169f4598fb9
Vishal-Borase <vishalborse2885@gmail.com>
2024-10-23 15:53:43 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,11 +10,8 @@ io.on("connection", async (socket) => {
   console.log("A user connected", socket.id);
 
   socket.on("disconnect", async () => {
-    console.log("User disconnected", socket.id);
     const user = await User.findOne({ socket_id: socket.id });
-    if (!user) {
-      return;
-    }
+    if (!user) return;
 
     io.sockets.in(user.chat_id).emit("user_left_frontend", {
       chat_id: user.chat_id,
@@ -31,15 +28,12 @@ io.on("connection", async (socket) => {
   });
 
   socket.on("join", async ({ user_name, user_id, chat_id, socket_id }) => {
-    console.log("User joined", user_name, user_id, chat_id, socket_id);
     socket.join(chat_id);
     const avatar = await getAvatar(user_id);
 
-    // Add user to the list of users in the chat room
-    const user = await User.findOne({ user_id });
-    if (user) {
-      console.log("User already exists");
-      await User.findByIdAndDelete(user._id);
+    const existingUser = await User.findOne({ user_id });
+    if (existingUser) {
+      await User.findByIdAndDelete(existingUser._id);
     }
 
     const newUser = new User({
@@ -51,39 +45,26 @@ io.on("connection", async (socket) => {
     });
     await newUser.save();
 
-    io.sockets
-      .in(chat_id)
-      .emit("user_joined", { user_name, user_id, avatar, chat_id });
-
-    io.sockets.in(chat_id).emit("update_users", {
-      chat_id: chat_id,
-      type: "joined",
-      user_name: user_name,
-    });
+    io.sockets.in(chat_id).emit("user_joined", { user_name, user_id, avatar, chat_id });
+    io.sockets.in(chat_id).emit("update_users", { chat_id, type: "joined", user_name });
   });
 
   socket.on("songEnded", async ({ _id }) => {
-    console.log("Song ended", _id);
-
     const song = await Music.findByIdAndDelete(_id);
-    if (!song) {
-      return;
-    }
+    if (!song) return;
+
     io.sockets.in(song.chat_id).emit("update_song", { chat_id: song.chat_id });
   });
 
   socket.on("songProgress", async ({ chat_id, timestamp }) => {
     try {
-      const musics = await Music.find({ chat_id });
-      if (musics) {
-        const music = musics[0];
-        if (music.timestamp < timestamp) {
-          music.timestamp = timestamp;
-
-          await music.save();
-          io.sockets
-            .in(chat_id)
-            .emit("update_song_progress", { chat_id, timestamp });
+      const songs = await Music.find({ chat_id });
+      if (songs.length > 0) {
+        const song = songs[0];
+        if (song.timestamp < timestamp) {
+          song.timestamp = timestamp;
+          await song.save();
+          io.sockets.in(chat_id).emit("update_song_progress", { chat_id, timestamp });
         }
       }
     } catch (error) {
